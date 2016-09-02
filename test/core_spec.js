2ab36c969d7ebe40276851020c0f9d70af10dbc2
tobyret <toby.retallick@gmail.com>
2016-09-02 23:24:54 +0100

    Fix failing test in core_spec.js

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
-import {List, Map} from "immutable"
-import {expect} from "chai"
-import {setEntries, next, vote} from "../src/core"
+import {List, Map, fromJS} from "immutable";
+import {expect} from "chai";
+import {setEntries, next, vote} from "../src/core";
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -95,51 +95,39 @@ describe('application logic', () => {
 
   describe('vote', () => {
     it('creates a tally for the voted entry', () => {
-      const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later')
-        }),
-        entries: List()
+      const state = fromJS({
+        pair: ['Trainspotting', '28 Days Later']
       })
 
       const nextState = vote(state, 'Trainspotting')
 
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({
-            'Trainspotting': 1
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 1
+        })
       }))
     })
-  })
 
-  it('adds to existing tally for the voted entry', () => {
-    const state = Map({
-      vote: Map({
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
         pair: List.of('Trainspotting', '28 Days Later'),
         tally: Map({
           'Trainspotting': 3,
           '28 Days Later': 2
         })
-      }),
-      entries: List()
-    })
+      })
 
-    const nextState = vote(state, 'Trainspotting')
+      const nextState = vote(state, 'Trainspotting')
 
-    expect(nextState).to.equal(Map({
-      vote: Map({
+      expect(nextState).to.equal(Map({
         pair: List.of('Trainspotting', '28 Days Later'),
         tally: Map({
           'Trainspotting': 4,
           '28 Days Later': 2
         })
-      }),
-      entries: List()
-    }))
+      }))
+    })
   })
 })
 
